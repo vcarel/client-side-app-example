d011507103ad0a01d2c9ee01931b83a55c7cc9a1
Vianney Carel <vcarel@localhost>
2014-10-06 14:57:32 +0200

    speed up basic tasks execution (especially the help)

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,6 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
-var extReplace = require('gulp-ext-replace');
-var jshint = require('gulp-jshint');
-var gzip = require('gulp-gzip');
-var minifyCSS = require('gulp-minify-css');
-var minifyHTML = require('gulp-minify-html');
-var mustache = require('gulp-mustache');
-var nib = require('nib');
-var print = require('gulp-print');
-var stylus = require('gulp-stylus');
-var tar = require('gulp-tar');
-var uglify = require('gulp-uglify');
-var webmake = require('gulp-webmake');
-
-var chalk = require('chalk');
 
 var pkg = require('./package.json');
-
 var dirs = {
     compile: 'build/compile/',
     minify: 'build/minify/',
@@ -26,6 +10,7 @@ var dirs = {
 
 
 gulp.task('default', function (done) {
+    var chalk = require('chalk');
     console.log('');
     console.log(chalk.bold('Available tasks'));
     console.log(chalk.cyan('      build') + ' Build and minify the application.');
@@ -37,14 +22,19 @@ gulp.task('default', function (done) {
     done();
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', ['build'], function (done) {
     gulp.watch('src/js/*', ['js']);
     gulp.watch('src/styl/*', ['css']);
     gulp.watch('src/main.mustache', ['html']);
     gulp.watch('src/img/*', ['img']);
+
+    var chalk = require('chalk');
+    console.log(chalk.yellow('Now watching source files...'));
+    done();
 });
 
 gulp.task('build', ['js', 'css', 'html', 'img'], function (done) {
+    var chalk = require('chalk');
     console.log(chalk.green('Successfully built application.') + ' The following files are available:');
     console.log(chalk.cyan('    build/compile/main.html') + ' unminified version for debugging purpose');
     console.log(chalk.cyan('     build/minify/main.html') + ' distributable version, minified');
@@ -52,16 +42,19 @@ gulp.task('build', ['js', 'css', 'html', 'img'], function (done) {
 });
 
 gulp.task('clean', function () {
+    var clean = require('gulp-clean');
     return gulp.src([dirs.compile, dirs.minify], {read: false})
         .pipe(clean());
 });
 
 gulp.task('clean-img', function () {
+    var clean = require('gulp-clean');
     return gulp.src([dirs.compile + dirs.resource + 'img', dirs.minify + dirs.resource + 'img'], {read: false})
         .pipe(clean());
 });
 
 gulp.task('jshint', function () {
+    var jshint = require('gulp-jshint');
     return gulp.src(['gulpfile.js', 'src/js/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
@@ -69,6 +62,8 @@ gulp.task('jshint', function () {
 });
 
 gulp.task('js', ['jshint'], function () {
+    var uglify = require('gulp-uglify');
+    var webmake = require('gulp-webmake');
     return gulp.src('src/js/main.js')
         .pipe(webmake())
         .pipe(gulp.dest(dirs.compile + dirs.resource))
@@ -77,6 +72,9 @@ gulp.task('js', ['jshint'], function () {
 });
 
 gulp.task('css', function () {
+    var minifyCSS = require('gulp-minify-css');
+    var nib = require('nib');
+    var stylus = require('gulp-stylus');
     return gulp.src('src/styl/main.styl')
         .pipe(stylus({
             use: nib()
@@ -87,6 +85,9 @@ gulp.task('css', function () {
 });
 
 gulp.task('html', function () {
+    var extReplace = require('gulp-ext-replace');
+    var minifyHTML = require('gulp-minify-html');
+    var mustache = require('gulp-mustache');
     return gulp.src('src/main.mustache')
         .pipe(mustache({
             css_dir: dirs.resource,
@@ -111,6 +112,9 @@ gulp.task('img', ['clean-img'], function () {
 });
 
 gulp.task('dist', ['build'], function () {
+    var gzip = require('gulp-gzip');
+    var print = require('gulp-print');
+    var tar = require('gulp-tar');
     return gulp.src(dirs.minify + '**')
         .pipe(tar(pkg.name + '_' + pkg.version + '.tar'))
         .pipe(gzip())
